298ec04a12dbaf87cf97d8cdca78b36f2e1446f7
aislamtime <116115530+aislamtime@users.noreply.github.com>
2022-11-21 22:40:50 +0300

    stylized the post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -7,9 +7,16 @@ type PropsType = {
 
 export function Post(props: PropsType) {
 	return (
-		<div className={s.item}>
-			<img src='' alt='' className={s.avatar} />
-			<div className={s.text}>{props.message}</div>
+		<div className={s.post}>
+			<div className={s.item}>
+				<img
+					src='https://i.pinimg.com/originals/2e/2e/21/2e2e2125ee53807c2d77b34773f84b5c.jpg'
+					alt=''
+					className={s.avatar}
+				/>
+				<div className={s.text}>{props.message}</div>
+				<div className={s.delete}>Delete post</div>
+			</div>
 		</div>
 	);
 }
