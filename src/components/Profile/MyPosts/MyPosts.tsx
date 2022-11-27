d930ddff90dc3f3f4de3b11532f8b6ed00612a7d
aislamtime <116115530+aislamtime@users.noreply.github.com>
2022-11-27 16:27:12 +0300

    mini refactor

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,33 +1,35 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
 import s from './MyPosts.module.css';
 import { PostType } from './../../../redux/state';
 import { Post } from './Post/Post';
 
 type MyPostsType = {
-  posts: Array<PostType>
-  addPost: (newMessage: string) => void
-}
+	posts: Array<PostType>;
+	addPost: (newMessage: string) => void;
+};
 
 export function MyPosts(props: MyPostsType) {
+	const postsElements = props.posts.map((el) => <Post state={el} />);
 
-  const postsElements = props.posts.map((el) => <Post state={el} />);
+	const textareaElementRef = React.createRef<HTMLTextAreaElement>();
 
-  const textareaElementRef = useRef<HTMLTextAreaElement | null>(null);
+	const onAddPostClickHandler = () => {
+		if (textareaElementRef.current) {
+			props.addPost(textareaElementRef.current.value);
+			textareaElementRef.current.value = '';
+		}
+	};
 
-  const onAddPostClickHandler = () => {
-    debugger;
-    props.addPost(textareaElementRef.current.value)
-    textareaElementRef.current.value = ''
-  }
-
-  return (
-    <div className={s.myPosts}>
-      <div className={s.myPostsTitle}>My posts</div>
-      <div className={s.inputAndButton}>
-        <textarea className={s.input} ref={textareaElementRef} ></textarea>
-        <button className={s.buttonSend} onClick={onAddPostClickHandler}>Send</button>
-      </div>
-      <div className={s.posts}>{postsElements}</div>
-    </div>
-  )
+	return (
+		<div className={s.myPosts}>
+			<div className={s.myPostsTitle}>My posts</div>
+			<div className={s.inputAndButton}>
+				<textarea className={s.input} ref={textareaElementRef}></textarea>
+				<button className={s.buttonSend} onClick={onAddPostClickHandler}>
+					Send
+				</button>
+			</div>
+			<div className={s.posts}>{postsElements}</div>
+		</div>
+	);
 }
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,4 +1,48 @@
-let rerenderEntireTree = () => {}
+let store = {
+    //_subscribe: function(observer) {
+        
+    //},
+	_state: {
+		profilePage: {
+			posts: [
+				{ id: 1, message: 'I am from Mars', likesCount: 13 },
+				{ id: 2, message: 'My name is Alesha', likesCount: 18 },
+				{ id: 3, message: 'Hi, how are you?', likesCount: 16 },
+				{ id: 4, message: "Hey, it's my first app! ", likesCount: 32 },
+			],
+		},
+		dialogsPage: {
+			dialogs: [
+				{ id: 1, name: 'Andrey' },
+				{ id: 2, name: 'Nikita' },
+				{ id: 3, name: 'Sasha' },
+				{ id: 4, name: 'Nika' },
+				{ id: 5, name: 'Tima' },
+				{ id: 6, name: 'Nastya' },
+			],
+			messages: [
+				{ id: 1, message: 'Hi!' },
+				{ id: 2, message: 'How are you?' },
+				{ id: 3, message: 'Yo! Wats up? ' },
+				{ id: 4, message: 'Yo' },
+			],
+		},
+	},
+	getState: function () {
+		return this._state;
+	},
+	addPost: function (newMessage: string) {
+		const newPost = {
+			id: 5,
+			message: newMessage,
+			likesCount: 0,
+		};
+		this._state.profilePage.posts.push(newPost);
+		rerenderEntireTree();
+	},
+};
+
+let rerenderEntireTree = () => {};
 
 export type MessageType = {
 	id: number;
@@ -24,7 +68,6 @@ export type RootStateType = {
 	profilePage: ProfilePageType;
 	dialogsPage: DialogsPageType;
 };
-
 
 export const state: RootStateType = {
 	profilePage: {
@@ -53,19 +96,16 @@ export const state: RootStateType = {
 	},
 };
 
-
-
 export const addPost = (newMessage: string) => {
-  debugger;
-  const newPost = {
-    id: 5,
-    message: newMessage,
-    likesCount: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  rerenderEntireTree()
+	const newPost = {
+		id: 5,
+		message: newMessage,
+		likesCount: 0,
+	};
+	state.profilePage.posts.push(newPost);
+	rerenderEntireTree();
 };
 
 export const subscribe = (observer: any) => {
-    rerenderEntireTree = observer
-} 
+	rerenderEntireTree = observer;
+};
